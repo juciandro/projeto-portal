17d03a75267369bca494bfb123eef150179adea9
Juciandro Andrade <juciandro59@gmail.com>
2018-02-15 03:07:31 -0400

    segunda parte do slide show concluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
-window.onload = function() {
+var slideItem=0;
+window.onload = function () {
+    setInterval('proximoSlide()', 4000);
     var slidewidth = document.getElementById("slide-show").offsetWidth;
     var objs = document.getElementsByClassName("slide");
-    for(var i=0;i<objs.length;i++){
-        objs[i].style.width = slidewidth+'px';
+    for (var i = 0; i < objs.length; i++) {
+        objs[i].style.width = slidewidth + 'px';
     }
+}
+function proximoSlide() {
+    var slidewidth = document.getElementById("slide-show").offsetWidth;
+    if (slideItem >= 3) {
+        slideItem = 0;
+    } else {
+        slideItem++;
+    }
+    document.getElementsByClassName("slideshow-area")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
